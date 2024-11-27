25efa119f199904974df788389c2946d1117966f
Huanhuynh24 <huanhuynh2402@gmail.com>
2024-11-27 09:35:59 +0700

    upload first

---
+++ BE/BE/BE/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace BE.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        // Phương thức GET đơn giản
+        [HttpGet]
+        public IActionResult GetGreeting()
+        {
+            return Ok("Xin chào!");
+        }
+    }
+}
