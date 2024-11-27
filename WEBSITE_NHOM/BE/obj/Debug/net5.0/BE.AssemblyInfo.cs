8f2204783a57799e99d727370066b912d97394a7
Huanhuynh24 <huanhuynh2402@gmail.com>
2024-11-27 11:25:14 +0700

    upload

--- WEBSITE_NHOM/BE/obj/Debug/net5.0/BE.AssemblyInfo.cs
+++ WEBSITE_NHOM/BE/obj/Debug/net5.0/BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb2464205a82208f7b73a60bbd4db9dd52e0666")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd8bd1b3f6a9ca9452fc0a2272e565aad13ec873")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
